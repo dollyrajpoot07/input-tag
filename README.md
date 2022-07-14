13b9e80fc5a00d08829efcb9298adf83982fbfec
Dolly Singh <dollyrajpoot07@gmail.com>
2022-07-14 16:35:15 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Input Tag
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/d011y_s1ngh/pen/eYWLVRd](https://codepen.io/d011y_s1ngh/pen/eYWLVRd).
+
---
+++ src/script.js
@@ -0,0 +1,24 @@
+const input = document.getElementById('yourInput');
+var listArray = [];
+input.addEventListener("keyup", function(e) {
+	if(e.keyCode === 13) {
+		let inValue = e.target.value;
+		listArray.push(inValue.replace(/\s/g,''));
+		var newTagList = '';
+		listArray.forEach(function(element, index) {
+			newTagList += `<span>#${element}<i class = "fa fa-times" onclick = "dlt(${index})"></i></span>`;
+		});
+		document.querySelector(".tag").innerHTML = newTagList;
+		input.value = '';
+	}
+});
+
+function dlt(index) {
+	listArray.splice(index, 1);
+	var newTagList = '';
+	listArray.forEach(element, index) => {
+		newTagList += `<span>#${element}<i class = "fa fa-times" onclick = "dlt(${index})"></i></span>`;
+	});
+	document.querySelector('.tag').innerHTML = newTagList;
+}
+
